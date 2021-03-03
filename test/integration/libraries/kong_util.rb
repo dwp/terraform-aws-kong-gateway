c55fd4329108e4f61574ac640369222eca4ed14d
Steve Brown <stephen.brown@konghq.com>
2021-03-03 07:27:31 +0000

    Added shared util library for tests, introduced a wait loop to check for kong clustering

---
+++ test/integration/libraries/kong_util.rb
@@ -0,0 +1,28 @@
+require 'net/http'
+require 'uri'
+
+def wait(url, max=500)
+  count = 0
+  while count <= max
+    begin
+      uri = URI.parse(url)
+      response = Net::HTTP.get_response(uri)
+      raise "Bad response from kong gateway: #{response.code}" if response.code.to_i != 200
+
+      raise 'empty cluster body' if JSON.parse(response.body).empty?
+
+      break
+    rescue Exception => e
+      count += 1
+      sleep 1
+      next
+    end
+  end
+end
+
+def post(url, data)
+  uri = URI.parse(url)
+  request = Net::HTTP::Post.new(uri)
+  request.set_form_data(data)
+  Net::HTTP.start(uri.hostname, uri.port).request(request)
+end
