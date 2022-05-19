768f6ba194e98be6b956cd8b497b0ad52481796a
Dan Webb <dan.webb@damacus.io>
2022-05-19 11:56:42 +0100

    Add control block for better descriptions
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -5,28 +5,44 @@ require_relative '../../libraries/kong_util'
 
 wait("#{api}/clustering/status")
 
-post("#{api}/services", { 'name' => 'test', 'url' => 'http://httpbin.org' })
-
-post("#{api}/services/test/routes", { 'name' => 'testRoute', 'paths' => '/test' })
-
-cluster_members = JSON.parse(http("#{api}/clustering/status", method: 'GET').body)
-
 describe cluster_members do
   it { should_not be_empty }
 end
 
-describe http("#{api}/services/test",
-              method: 'GET') do
-                its('status') { should cmp 200 }
-              end
+control 'Cluster Size' do
+  impact 1
+  title 'Cluster should not be empty'
+  describe http("#{api}/clustering/status") do
+    its('body') { should_not be_empty }
+    its('status') { should cmp 200 }
+  end
+end
 
-describe http("#{api}/services/test/routes/testRoute",
-              method: 'GET') do
-                its('status') { should cmp 200 }
-              end
 
-sleep(10) # wait for route to propergate
-describe http("#{proxy}/test/get",
-              method: 'GET') do
-                its('status') { should cmp 200 }
-              end
+describe http("#{api}/services/test") do
+  its('status') { should cmp 200 }
+end
+
+control 'Test Services' do
+  impact 0.7
+  title 'Services should be available'
+
+  describe http("#{api}/services/test") do
+    its('status') { should cmp 200 }
+  end
+end
+
+# sleep(10) # wait for route to propagate
+
+control 'Working route' do
+  impact 1
+  title 'The test route should be working'
+
+  describe http("#{api}/services/test/routes/testRoute") do
+    its('status') { should cmp 200 }
+  end
+
+  describe http("#{proxy}/test/get") do
+    its('status') { should cmp 200 }
+  end
+end
