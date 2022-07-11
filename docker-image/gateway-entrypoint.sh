82e21e761e5022b5296044cfc62262d4d80fd0d8
Daniel.Hill <daniel.hill@engineering.digital.dwp.gov.uk>
2022-07-11 21:01:28 +0100

    feat(docker-image): add docker image and job to push to ghcr
    Signed-off-by: Daniel.Hill <daniel.hill@engineering.digital.dwp.gov.uk>

---
+++ docker-image/gateway-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -Eeo pipefail
+
+export KONG_NGINX_DAEMON=${KONG_NGINX_DAEMON:=off}
+
+PREFIX=${KONG_PREFIX:=/usr/local/kong}
+
+set +x
+mkdir -p /usr/local/kong/ssl
+echo "${SSL_CERT}" > /usr/local/kong/ssl/kong.crt
+echo "${SSL_KEY}"  > /usr/local/kong/ssl/kong.key
+
+echo "${LUA_SSL_CERT}" > /usr/local/kong/ssl/lua.crt
+
+mkdir -p /usr/local/kong/kong_clustering
+echo "${CLUSTER_CERT}" > /usr/local/kong/kong_clustering/cluster.crt
+echo "${CLUSTER_CA}" > /usr/local/kong/kong_clustering/cluster_ca.crt
+echo "${CLUSTER_KEY}" > /usr/local/kong/kong_clustering/cluster.key
+echo "${CUSTOM_NGINX_CONF}" | base64 -d > /usr/local/kong/custom-nginx.conf
+
+kong prepare -p "$PREFIX" "$@"
+
+ln -sf /dev/stdout $PREFIX/logs/access.log
+ln -sf /dev/stdout $PREFIX/logs/admin_access.log
+ln -sf /dev/stderr $PREFIX/logs/error.log
+
+exec kong start -p "$PREFIX" "$@"
