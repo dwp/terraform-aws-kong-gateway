77691a39e8498071e6c6bb0e3932b0a234af23db
Daniel.Hill <daniel.hill@engineering.digital.dwp.gov.uk>
2022-07-11 20:32:35 +0100

    chore(*): reuse InSpec tests in each Kitchen suite
    Instead of maintaining multiple copies of the tests, use the `default` tests in each suite

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -5,16 +5,15 @@ require_relative '../../libraries/kong_util'
 
 wait("#{api}/clustering/status") # wait for Kong to be running
 
-control 'Cluster Size' do
-    impact 1
-    title 'Create service and route'
-    describe http("#{api}/services", method: 'POST', data: { 'name' => 'test', 'url' => 'http://httpbin.org' }) do
-      its('status') { should cmp 200 }
-    end
-    describe http("#{api}/services/test", method: 'POST', data: { 'name' => 'test', 'url' => 'http://httpbin.org' }) do
-      its('status') { should cmp 200 }
-    end
-    sleep(10) # wait for route to propagate
+control 'Create service and route' do
+  impact 1
+  title 'Creating service and route should succeed'
+  describe http("#{api}/services/test", method: 'PUT', data: { 'name' => 'test', 'url' => 'http://httpbin.org' }) do
+    its('status') { should be_in [ 200, 201 ] }
+  end
+  describe http("#{api}/services/test/routes/testRoute", method: 'PUT', data: { 'name' => 'testRoute', 'paths' => '/test' }) do
+    its('status') { should be_in [ 200, 201 ] }
+  end
 end
 
 control 'Cluster Size' do
@@ -26,24 +25,27 @@ control 'Cluster Size' do
   end
 end
 
-control 'Test Services' do
+control 'Test Service' do
   impact 0.7
-  title 'Services should be available'
+  title 'Service should be available'
 
   describe http("#{api}/services/test") do
     its('status') { should cmp 200 }
   end
 end
 
-control 'Working route' do
+control 'Test route' do
   impact 1
   title 'The test route should be working'
 
-  describe http("#{api}/services/test/routes/testRoute") do
+  post("#{api}/services", { 'name' => 'httpbin', 'url' => 'http://httpbin.org' })
+  post("#{api}/services/httpbin/routes", { 'name' => 'httpbin', 'paths' => '/httpbin' })
+  sleep(10) # wait for route to propagate
+
+  describe http("#{api}/services/httpbin/routes/httpbin") do
     its('status') { should cmp 200 }
   end
-
-  describe http("#{proxy}/test/get") do
+  describe http("#{proxy}/httpbin/get") do
     its('status') { should cmp 200 }
   end
 end
